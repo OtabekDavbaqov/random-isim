3c220e433a5a68d2e13000e43b6407a3f197f8a6
OtabekDavbaqov <166129015+OtabekDavbaqov@users.noreply.github.com>
2024-05-27 18:51:44 +0500

    Add files via upload

---
+++ takrorlash/index.js
@@ -0,0 +1,8 @@
+var isimlar = [
+    "Otabek", "Anvar","Gulsevar","Husan","Ziyoda",
+  "Shabona","Xussnidin"
+]
+var random = Math.floor(Math.random()*isimlar.length);
+var random_2 = isimlar[random];
+console.log("Tanlangan isim bu: " + random_2);
+document.write("Tanlangan isim bu: " + random_2)
